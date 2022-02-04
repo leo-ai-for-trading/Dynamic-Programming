99adcdf103a9687720d6838af6d59cee42476e8c
Attilio Pittelli <70145737+leo-ai-for-trading@users.noreply.github.com>
2022-02-04 18:22:24 +0100

    Update 64. Minimum Path Sum.py

--- Medium/64. Minimum Path Sum/64. Minimum Path Sum.py
+++ Medium/64. Minimum Path Sum/64. Minimum Path Sum.py
@@ -1,1 +1,16 @@
 
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        '''
+        Reasoning
+        - min of the bottom square and the the right and compare them
+        - O(n*m)
+        '''
+        rows, cols = len(grid), len(grid[0])
+                #inizialize the value with inf
+        res = [[float("inf")]* (cols+1) for r in range(rows+1)] 
+        res[rows - 1][cols] = 0
+        for r in range(rows -1, -1, -1):
+            for c in range(cols -1 ,-1, -1):
+                res[r][c] = grid[r][c] + min(res[r + 1][c], res[r][c + 1])
+        return res[0][0]
