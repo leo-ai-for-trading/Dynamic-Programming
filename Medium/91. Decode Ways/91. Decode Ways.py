6f7d16fd32cbcaeca0f9b12650ed121f5842eeba
Attilio Pittelli <70145737+leo-ai-for-trading@users.noreply.github.com>
2022-02-15 11:17:41 +0100

    Update 91. Decode Ways.py

--- Medium/91. Decode Ways/91. Decode Ways.py
+++ Medium/91. Decode Ways/91. Decode Ways.py
@@ -3,15 +3,16 @@ class Solution:
         '''
         Reasoning
         - with DP like fibonacci sequence
-        - 
+        - bottom up approach starting at the end
         '''
         dp = {len(s):1}
         #base case
         
         for i in range(len(s) -1, -1, -1):
+            #iterating in reverse order
             if s[i] == "0":
                 dp[i] = 0
-            else:
+            else: #digit from 1 to 9
                 dp[i] = dp[i+1]
             if (i + 1 < len(s) and (s[i] == "1" or s[i] == "2" and s[i+1] in "0123456")):
                 dp[i] += dp[i+2]
