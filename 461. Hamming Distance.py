ae1a096d5555b9f9c07a98b76e5a073b3dcbdce4
Attilio Pittelli <70145737+leo-ai-for-trading@users.noreply.github.com>
2022-04-20 11:35:13 +0200

    Delete 461. Hamming Distance.py

--- 461. Hamming Distance.py
+++
@@ -1,4 +0,0 @@
-class Solution:
-    def hammingDistance(self, x: int, y: int) -> int:
-        xor = bin(x ^ y) # binary rapresentation
-        return xor.count('1') # xor result will be 1 if bits are different at that index
